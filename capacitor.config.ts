68ab16b83b1d79a5bf50c2eacd0fe7f7e937e8b1
Ryan <Ryan@localhost.com>
2023-10-23 21:27:48 +0700

    The login works fine together with the logo dark/light

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Training-Day-FE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  form = new FormGroup({
+    "email": new FormControl('', [Validators.required, Validators.email]),
+    "password": new FormControl('', Validators.required)
+  })
+  constructor(
+  ) { }
 
   ngOnInit() {
+    console.log(document.body.classList)
   }
 
+  submit(){
+
+  }
+
+  getLogoSrc(){
+    let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+    if(darkMode)
+      return "assets/logo-dark.png"
+    else
+      return "assets/logo-light.png"
+  }
+
+
+
+  protected readonly document = document;
 }
