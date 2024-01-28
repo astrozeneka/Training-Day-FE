71d79360deeccd456d7c58e3fe05c9e0f7b2a909
Ryan <Ryan@localhost.com>
2024-01-28 17:00:56 +0700

    Install the android dependency

--- android/app/src/main/java/com/trainingday/MainActivity.java
+++ android/app/src/main/java/com/trainingday/MainActivity.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.perf.metrics.AddTrace;
 
 public class MainActivity extends BridgeActivity {
-  @AddTrace(name = "onCreateTrace", enabled = true /* optional */)
   @Override
   public void onCreate(Bundle savedInstanceState) {
     System.out.println("Hello world");
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 ## การใช้ `ng build`
 ใช้คำสั่งนี้เพื่อติดตั้ง App บน low-memory เสอร์เวอร์
 ```shell
-node --max_old_space_size=3000 ./node_modules/@angular/cli/bin/ng.js build
+node --max_old_space_size=3000 ./node_modules/@angular/cli/bin/ng.js build --configuration=production
 
 cat <<EOF > "www/.htaccess"
 RewriteEngine on
