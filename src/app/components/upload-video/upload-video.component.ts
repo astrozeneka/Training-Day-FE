9c0d10dbd53bbc8ceaf1f7242bc24aa18b5ef629
Ryan <Ryan@localhost.com>
2024-03-09 16:15:40 +0700

    The video upload system works

--- src/app/components/upload-video/upload-video.component.ts
+++ src/app/components/upload-video/upload-video.component.ts
@@ -1,6 +1,6 @@
 import {Component, forwardRef, Input, OnInit, ViewChild} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpEventType, HttpHeaders, HttpRequest} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {ContentService} from "../../content.service";
 
@@ -16,11 +16,15 @@ import {ContentService} from "../../content.service";
     },
   ],
 })
-export class UploadVideoComponent  implements OnInit {
+export class UploadVideoComponent  implements ControlValueAccessor, OnInit {
 
   @ViewChild('fileInput') fileInput: any = undefined;
 
   @Input() label: string = "Ajouter une vidéo";
+
+  @Input() formControl: any = undefined;
+
+  progress = 0;
 
   constructor(
     private http: HttpClient,
@@ -40,15 +44,67 @@ export class UploadVideoComponent  implements OnInit {
     const reader = new FileReader();
     reader.onload = () => {
       const videoData = reader.result;
-      this.contentService.post('/video', {'video':{
+      let headers = this.contentService.bearerHeaders()
+      headers['Content-Type'] = 'application/json'
+      console.log(headers)
+      const uploadReq = new HttpRequest(
+        'POST',
+        `${this.contentService.apiEndpoint}/video`, JSON.stringify({"video": {
+          "name": file.name,
+          "type": file.type,
+          "base64": videoData,
+        }}),{
+          headers: new HttpHeaders(headers),
+          reportProgress: true,
+        }
+      );
+      console.log(uploadReq)
+
+      this.http.request(uploadReq).subscribe((event:any) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.progress = Math.round(100 * event.loaded / event.total);
+          console.log('Upload Progress: ' + Math.round(event.loaded / event.total * 100) + '%');
+        }else if (event.type === HttpEventType.Response) {
+          // Update the component value
+          this.formControl.setValue(event.body);
+          this.onChange(event.body);
+        }
+      })
+
+      /*this.http.post(`${environment.apiEndpoint}/video`, {'video':{
+          "name": file.name,
+          "type": file.type,
+          "base64": videoData,
+        }}, {headers})*/
+
+      /*this.contentService.post('/video', {'video':{
         "name": file.name,
         "type": file.type,
         "base64": videoData,
       }}).subscribe((data)=>{
         console.log(data);
-      });
+      });*/
     }
     reader.readAsDataURL(file);
   }
 
+  clear(){
+    this.fileInput.nativeElement.value = null;
+  }
+
+  onChange: any = () => {};
+
+  onTouch: any = () => {};
+
+  writeValue(value: any) {
+    // No need to store as it is already stored as a property of the formControl
+  }
+
+  registerOnChange(fn:any) {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouch = fn
+  }
 }
--- src/app/pages/video-upload/video-upload.page.ts
+++ src/app/pages/video-upload/video-upload.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormComponent} from "../../components/form.component";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ContentService} from "../../content.service";
+import {FeedbackService} from "../../feedback.service";
 
 @Component({
   selector: 'app-video-upload',
@@ -9,10 +12,13 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class VideoUploadPage extends FormComponent{
 
+  fileControl = new FormControl(null, [Validators.required]);
   override form = new FormGroup({
-    'title': new FormControl('', []),
-    'description': new FormControl('', []),
+    'title': new FormControl('', [Validators.required]),
+    'description': new FormControl('', [Validators.required]),
+    'file': this.fileControl
   });
+  valid = false;
 
   override displayedError = {
     'title': undefined,
@@ -20,15 +26,33 @@ export class VideoUploadPage extends FormComponent{
   }
 
   constructor(
+    private router:Router,
+    private contentService: ContentService,
+    private feedbackService: FeedbackService
   ) {
     super();
+    this.form.valueChanges.subscribe((value)=>{
+      this.valid = this.form.valid
+    })
   }
 
   ngOnInit() {
   }
 
   submit(){
-
+    let data:any = this.form.value
+    data.file_id = data.file.id
+    this.contentService.post('/video', data)
+      .subscribe((response:any)=>{
+        console.log('response')
+        // If the return object has an id, make it success
+        if(response.id){
+          this.feedbackService.register('Votre vidéo a été ajoutée avec succès', 'success')
+          this.router.navigate(['/home'])
+        }else{
+          this.feedbackService.registerNow('Erreur lors de l\'ajout de la vidéo', 'danger')
+        }
+      })
   }
 
 }
