2517964f94f207285d4cdf1974cafed314fb8fa8
Ryan <Ryan@localhost.com>
2024-12-05 07:52:19 +0300

    Allow the store to invite the user to login if he is not connected

--- src/app/components/storefront-item/storefront-item.component.ts
+++ src/app/components/storefront-item/storefront-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ContentService } from 'src/app/content.service';
 import { Product } from 'src/app/custom-plugins/store.plugin';
+import { User } from 'src/app/models/Interfaces';
 
 @Component({
   selector: 'app-storefront-item',
@@ -10,11 +12,18 @@ export class StorefrontItemComponent  implements OnInit {
   @Input() color: string = 'primary';
   @Input() disabled: boolean = false;
   @Output() action: EventEmitter<any> = new EventEmitter();
+  loggedIn: boolean = false;
   
   // The IAP produc to display
   @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private cs: ContentService // Low coupling strategy
+  ) { 
+    cs.userStorageObservable.gso$().subscribe((user:User) => {
+      this.loggedIn = !!user
+    })
+  }
 
   ngOnInit() {}
 
